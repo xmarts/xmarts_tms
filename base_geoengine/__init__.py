c10445f756c6a4fa68c3a32dafc3e91e93c1d1b6
Pablo Osorio <pablo.osorio@xmarts.com>
2019-02-19 10:03:33 -0600

    quite modulo base_geoengine por temas de compatibilidad en sh

--- base_geoengine/__init__.py
+++
@@ -1,8 +0,0 @@
-"""The GeoEngine module"""
-from . import geo_model
-from . import geo_operators
-from . import geo_view
-from . import geo_helper
-from . import geo_ir
-from . import fields
-from .geo_db import init_postgis
