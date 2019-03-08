aa8fa79b30b95ebe2fa55582217f049abb94cc03
Pablo Osorio <pabloosoriogama@gmail.com>
2019-03-08 00:47:48 -0600

    cambiando modelo del lugar y haciendo calculo de costo de casetas

--- x_tms/models/tms_route_tollstation.py
+++ x_tms/models/tms_route_tollstation.py
@@ -3,14 +3,14 @@
 # Copyright 2016, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class TmsRouteTollstation(models.Model):
     _name = 'tms.route.tollstation'
 
     name = fields.Char(required=True)
-    place_id = fields.Many2one('tms.place', string="Place", required=True)
+    place_id = fields.Many2one('res.colonia.zip.sat.code', string="Place", required=True)
     partner_id = fields.Many2one('res.partner', string="Partner")
     route_ids = fields.Many2many('tms.route', string="Routes")
     credit = fields.Boolean()
@@ -19,3 +19,18 @@ class TmsRouteTollstation(models.Model):
         'tollstation_id',
         string='Cost per Axis')
     active = fields.Boolean(default=True)
+
+    ejes = fields.Many2one("tms.route.tollstation.costperaxis",string="# Ejes")
+    costo_caseta = fields.Float(string="Costo caseta", compute="get_costo")
+    total_casetas = fields.Float(string="Total Casetas", compute="get_t_casetas")
+
+    @api.multi
+    @api.depends('credit','costo_caseta','ejes')
+    def get_costo(self):
+        for record in self:
+            costo = 0.0
+            if record.credit == True:
+                costo = record.ejes.cost_credit
+            else:
+                costo = record.ejes.cost_cash
+            record.costo_caseta = costo
