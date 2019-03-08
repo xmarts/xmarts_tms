5f206f8d8760b81000037c8c108b45f4f6a55dfe
Pablo Osorio <pabloosoriogama@gmail.com>
2019-03-08 00:48:40 -0600

    cambiando nombre a mostrar

--- x_tms/models/tms_route_tollstation_costperaxis.py
+++ x_tms/models/tms_route_tollstation_costperaxis.py
@@ -3,15 +3,28 @@
 # Copyright 2016, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class TmsRouteTollstationCostperaxis(models.Model):
     _name = 'tms.route.tollstation.costperaxis'
-
+    name = fields.Char(string="Ejes", compute="get_name")
     axis = fields.Integer(required=True)
     cost_credit = fields.Float(required=True)
     cost_cash = fields.Float(required=True)
     tollstation_id = fields.Many2one(
         'tms.route.tollstation',
         string='Toll Station')
+
+    @api.multi
+    @api.depends('name','axis')
+    def get_name(self):
+
+        res = []
+        for record in self:
+            name = ''
+            if record.axis == 1:
+                name = 'eje'
+            else:
+                name = 'ejes'
+            record.name = str(record.axis) + ' ' + name
