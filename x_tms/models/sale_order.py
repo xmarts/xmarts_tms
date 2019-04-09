483d44fd239d688fb2a83e782f17699559a30518
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-04-09 18:08:02 -0500

    se quito funcion no necesaria

--- x_tms/models/sale_order.py
+++ x_tms/models/sale_order.py
@@ -13,51 +13,51 @@ class SaleOrder(models.Model):
 
 
 
-	@api.depends('order_line')
-	@api.onchange('ruta')
-	def _onchange_ruta_pedido(self):
-		# product_caseta_obj = self.env['product.product'].search([('es_caseta','=',True)], limit=1)
-		product_factor_obj = self.env['product.product'].search([('es_factor_op','=',True)], limit=1)
-		product_combustible_obj = self.env['product.product'].search([('es_combustible','=',True)], limit=1)
-		line_ids = []
-		res = {'value':{
-                'order_line':[],
-            }
-		}
-		# sum_casetas = 0
-		# for sca in self.ruta.tollstation_ids:
-		# 	sum_casetas += sca.costo_caseta
-		# lineca = {
-  #         'product_id': product_caseta_obj.id,
-		#   'product_uom': product_caseta_obj.uom_id.id,
-		#   'name': 'Costo de las casetas generado en la ruta',
-		#   'price_unit': sum_casetas,
-		#   'product_uom_qty':1
-  #       }
-		sum_factor = 0
-		for sca in self.ruta.driver_factor_ids:
-			sum_factor += sca.fixed_amount
-		linefa = {
-          'product_id': product_factor_obj.id,
-		  'product_uom': product_factor_obj.uom_id.id,
-		  'name': 'Costo del operador generado en la ruta',
-		  'price_unit': sum_factor,
-		  'product_uom_qty':1
-        }
-		# sum_combustible = 0
-		# for sca in self.ruta.fuel_efficiency_ids:
-		# 	sum_combustible += sca.performance
-		# lineco = {
-  #         'product_id': product_combustible_obj.id,
-		#   'product_uom': product_combustible_obj.uom_id.id,
-		#   'name': 'Costo del combustible generado en la ruta',
-		#   'price_unit': sum_combustible,
-		#   'product_uom_qty':1
-  #       }
-		# line_ids += [lineca]
-		line_ids += [linefa]
-		# line_ids += [lineco]
-		res['value'].update({
-            'order_line': line_ids,
-		})
-		return res
+	# @api.depends('order_line')
+	# @api.onchange('ruta')
+	# def _onchange_ruta_pedido(self):
+	# 	# product_caseta_obj = self.env['product.product'].search([('es_caseta','=',True)], limit=1)
+	# 	product_factor_obj = self.env['product.product'].search([('es_factor_op','=',True)], limit=1)
+	# 	product_combustible_obj = self.env['product.product'].search([('es_combustible','=',True)], limit=1)
+	# 	line_ids = []
+	# 	res = {'value':{
+ #                'order_line':[],
+ #            }
+	# 	}
+	# 	# sum_casetas = 0
+	# 	# for sca in self.ruta.tollstation_ids:
+	# 	# 	sum_casetas += sca.costo_caseta
+	# 	# lineca = {
+ #  #         'product_id': product_caseta_obj.id,
+	# 	#   'product_uom': product_caseta_obj.uom_id.id,
+	# 	#   'name': 'Costo de las casetas generado en la ruta',
+	# 	#   'price_unit': sum_casetas,
+	# 	#   'product_uom_qty':1
+ #  #       }
+	# 	sum_factor = 0
+	# 	for sca in self.ruta.driver_factor_ids:
+	# 		sum_factor += sca.fixed_amount
+	# 	linefa = {
+ #          'product_id': product_factor_obj.id,
+	# 	  'product_uom': product_factor_obj.uom_id.id,
+	# 	  'name': 'Costo del operador generado en la ruta',
+	# 	  'price_unit': sum_factor,
+	# 	  'product_uom_qty':1
+ #        }
+	# 	# sum_combustible = 0
+	# 	# for sca in self.ruta.fuel_efficiency_ids:
+	# 	# 	sum_combustible += sca.performance
+	# 	# lineco = {
+ #  #         'product_id': product_combustible_obj.id,
+	# 	#   'product_uom': product_combustible_obj.uom_id.id,
+	# 	#   'name': 'Costo del combustible generado en la ruta',
+	# 	#   'price_unit': sum_combustible,
+	# 	#   'product_uom_qty':1
+ #  #       }
+	# 	# line_ids += [lineca]
+	# 	line_ids += [linefa]
+	# 	# line_ids += [lineco]
+	# 	res['value'].update({
+ #            'order_line': line_ids,
+	# 	})
+	# 	return res
