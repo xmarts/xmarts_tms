2de6297d4627140e8a5b33f6fa52f5223bc49856
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-04-22 12:06:38 -0500

    test

--- x_tms/models/testkl.py
+++ x_tms/models/testkl.py
@@ -3,32 +3,59 @@
 from datetime import datetime, timedelta
 import mygeotab
 
-username = 'javier.ramirez@sli.mx'
-password = 'Javier0318*'
-database = 'GSYEECA'
+# username = 'javier.ramirez@sli.mx'
+# password = 'Javier0318*'
+# database = 'GSYEECA'
 
-geo = mygeotab.API(
-        username=username,
-        password=password,
-        database=database
-    )
+# geo = mygeotab.API(
+#         username=username,
+#         password=password,
+#         database=database
+#     )
 
-authenticate = geo.authenticate()
-
-
+# authenticate = geo.authenticate()
 
 
 
-def calculate_litres_per_100km(from_date, to_date):
 
+
+# def calculate_litres_per_100km(from_date, to_date):
+
+#     odometer_records = geo.get('StatusData', 
+#                                   diagnosticSearch=dict(id='DiagnosticOdometerAdjustmentId'),
+#                                   deviceSearch=dict(engineVehicleIdentificationNumber='3HSDJAPT4HN513265'),
+#                                   toDate=to_date,
+#                                   fromDate=from_date)
+#     fuel_records = geo.get('StatusData',
+#                               diagnosticSearch=dict(id='DiagnosticDeviceTotalFuelId'),
+#                               deviceSearch=dict(engineVehicleIdentificationNumber='3HSDJAPT4HN513265'),
+#                               toDate=to_date,
+#                               fromDate=from_date)
+#     if len(odometer_records) == 0 or len(fuel_records) == 0:
+#         raise Exception('Device has not travelled in this time period or no fuel usage reported')
+#     odometer_change = odometer_records[-1]['data'] - odometer_records[0]['data']
+#     fuel_change = fuel_records[-1]['data'] - fuel_records[0]['data']
+#     return fuel_change / (odometer_change / 1000)
+
+def calculate_litres_per_km(from_date, to_date, serial):
+    username = 'javier.ramirez@sli.mx'
+    password = 'Javier0318*'
+    database = 'GSYEECA'
+    geo = mygeotab.API(
+            username=username,
+            password=password,
+            database=database
+        )
+    authenticate = geo.authenticate()
+    xxx = geo.get('Device', engineVehicleIdentificationNumber=serial)[0]
     odometer_records = geo.get('StatusData', 
                                   diagnosticSearch=dict(id='DiagnosticOdometerAdjustmentId'),
-                                  deviceSearch=dict(engineVehicleIdentificationNumber='3HSDJAPT4HN513265'),
+                                  deviceSearch=dict(id=xxx['id']),
                                   toDate=to_date,
                                   fromDate=from_date)
     fuel_records = geo.get('StatusData',
                               diagnosticSearch=dict(id='DiagnosticDeviceTotalFuelId'),
-                              deviceSearch=dict(engineVehicleIdentificationNumber='3HSDJAPT4HN513265'),
+                              deviceSearch=dict(id=xxx['id']),
                               toDate=to_date,
                               fromDate=from_date)
     if len(odometer_records) == 0 or len(fuel_records) == 0:
@@ -41,7 +68,7 @@ def calculate_litres_per_100km(from_date, to_date):
 # start = end - timedelta(days=365)
 #xxx = geo.get('Device', engineVehicleIdentificationNumber='3HSDJAPT4HN513265')[0]
 #print(xxx)
-print(calculate_litres_per_100km(datetime.utcnow() - timedelta(days=10), datetime.utcnow()))
+print(calculate_litres_per_km(datetime.utcnow() - timedelta(days=3), datetime.utcnow(),'3HSDJAPT4HN513265'))
 
 
 
