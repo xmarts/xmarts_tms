2cd3c4b719bbc4aad161db8ac7c3670c47986c50
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-06-06 10:05:20 -0500

    remove innecesari files

--- add_tms_r/models/tms_employee_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class tms_categories_events(models.Model):
-
-
-	_name = 'hr.employee.category'
-	name = fields.Char('Etiqueta del empleado')
