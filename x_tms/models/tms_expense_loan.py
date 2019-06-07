efaa38185900b2e0f674d670edf042d3ebb2ff65
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-06-07 11:59:06 -0500

    x

--- x_tms/models/tms_expense_loan.py
+++ x_tms/models/tms_expense_loan.py
@@ -215,7 +215,7 @@ class TmsExpenseLoan(models.Model):
                 for line in loan.expense_ids:
                     line_amount += line.price_total
                 loan.balance = loan.amount + line_amount
-            if loan.balance <= 0.0:
+            if loan.balance <= 0.0 and loan.state == 'confirmed':
                 loan.state = 'closed'
                 #loan.write({'state': 'closed'})
 
