9b0c19918ad3196660418573791b7b8e9f7b7c39
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-07-08 16:50:31 -0500

    .

--- x_tms/models/account_invoice.py
+++ x_tms/models/account_invoice.py
@@ -25,6 +25,45 @@ class AccountInvoice_travel(models.Model):
     line = fields.Boolean(default=False)
     account=fields.Many2one('account.invoice', string='Factura')
 
+    # @api.multi
+    # def write(self, vals):
+    #     for rec in self:
+    #         res = super(AccountInvoice_travel, self).write(vals)
+    #         product_felte_obj = self.env['product.product'].search([('es_flete','=',True)], limit=1)
+    #         account_analytic = self.env['account.analytic.account'].search([('name','=',rec.travel_id.unit_id.name)], limit=1)
+    #         if not account_analytic:
+    #             account_analytic=self.env['account.analytic.account'].create({                      
+    #                 'name':rec.travel_id.unit_id.name,                             
+    #                 })
+    #         taxes = []
+    #         for t in product_felte_obj.taxes_id:
+    #             taxes.append(t.id)  
+    #         self.env['account.invoice.line'].create({           
+    #         #'sequence':10,
+    #         'product_id':product_felte_obj.id,
+    #         'origin':'Cobro por el flete del viaje - ' + rec.travel_id.name,
+    #         'name':'Cobro por el flete del viaje - ' + rec.travel_id.name,
+    #         'company_id':rec.account.company_id.id,
+    #         'invoice_id':rec.account.id,
+    #         'travels_ids':rec.id,
+    #         'account_id':product_felte_obj.categ_id.property_account_income_categ_id.id,
+    #         'account_analytic_id':account_analytic.id,
+    #         #'analytic_tag_ids':rec.
+    #         'quantity':1.00,
+    #         'uom_id':product_felte_obj.uom_id.id,
+    #         'price_unit':rec.travel_id.flete_cliente,
+    #         #'discount':0.00,
+    #         'invoice_line_tax_ids':[(6, 0, taxes)],
+    #         #'price_subtotal':100.00,
+    #         #'currency_id':34
+    #         })
+
+           
+    #         rec.write({'line':True})
+
+    #         rec.write({'account_id':rec.account.id})
+    #         return res
+
     @api.multi
     def unlink(self):
         for rec in self:
@@ -32,7 +71,7 @@ class AccountInvoice_travel(models.Model):
 
             for acc in rec.travel_id:
                 acc.write({'account_id':False})
-            self.account._onchange_invoice_line_ids()     
+            #self.account._onchange_invoice_line_ids()     
             return super(AccountInvoice_travel, self).unlink()
 
 class AccountInvoice_line(models.Model):
@@ -48,34 +87,96 @@ class AccountInvoice(models.Model):
         'tms.waybill', 'invoice_id', string="Waybills", readonly=True)
     travel_ids = fields.One2many('account.invoice.travel', 'account', string='Travels')
 
-    @api.onchange('travel_ids')
-    def _onchange_travel_ids(self):
-        taxes_grouped = self.get_taxes_values()
-        tax_lines = self.tax_line_ids.filtered('manual')
-        for tax in taxes_grouped.values():
-            tax_lines += tax_lines.new(tax)
-        self.tax_line_ids = tax_lines
-        return
+    # @api.onchange('invoice_line_ids')
+    # def _onchange_travel_ids(self):
+    #     taxes_grouped = self.get_taxes_values()
+    #     tax_lines = self.tax_line_ids.filtered('manual')
+    #     for tax in taxes_grouped.values():
+    #         tax_lines += tax_lines.new(tax)
+    #     self.tax_line_ids = tax_lines
+    #     return
+
+    # @api.depends('invoice_line_ids')
+    # @api.multi
+    # def calcula_inv_lines(self):
+    #     for rec in self:
+    #         for x in rec.invoice_line_ids:
+    #             if x.travels_ids:
+    #                 x.quantity = 1
+
+    # @api.onchange('travel_ids')
+    # def _onchange_invoice_line_idss(self):
+    #     taxes_grouped = {}
+    #     round_curr = self.currency_id.round
+    #     for line in self.travel_ids:
+    #         price_unit = line.travel_id.flete_cliente 
+    #         product_felte_obj = self.env['product.product'].search([('es_flete','=',True)], limit=1)
+    #         taxes = product_felte_obj.taxes_id.compute_all(price_unit, self.currency_id, 1, product_felte_obj, self.partner_id)['taxes']
+    #         for tax in taxes:
+    #             val = self._prepare_tax_line_vals(line, tax)
+    #             key = self.env['account.tax'].browse(tax['id']).get_grouping_key(val)
+
+    #             if key not in tax_grouped:
+    #                 tax_grouped[key] = val
+    #                 tax_grouped[key]['base'] = round_curr(val['base'])
+    #             else:
+    #                 tax_grouped[key]['amount'] += val['amount']
+    #                 tax_grouped[key]['base'] += round_curr(val['base'])
+    #     tax_lines = self.tax_line_ids.filtered('manual')
+    #     for tax in taxes_grouped.values():
+    #         tax_lines += tax_lines.new(tax)
+    #     self.tax_line_ids = tax_lines
+    #     return
 
     @api.multi
-    def write(self, values):
-
+    def update_invoice_lines(self):
         for rec in self:
             #rec.travel_ids.account._onchange_invoice_line_ids()
-            res = super(AccountInvoice, self).write(values)            
-            if res:
-                product_felte_obj = self.env['product.product'].search([('es_flete','=',True)], limit=1)
+                       
+            product_felte_obj = self.env['product.product'].search([('es_flete','=',True)], limit=1)
 
-                for recs in rec.travel_ids:
+            for recs in rec.travel_ids:
 
-                    for acc in recs.travel_id:
-                        account_analytic = self.env['account.analytic.account'].search([('name','=',acc.unit_id.name)], limit=1)
-                        if account_analytic:
+                for acc in recs.travel_id:
+                    account_analytic = self.env['account.analytic.account'].search([('name','=',acc.unit_id.name)], limit=1)
+                    if account_analytic:
+                        taxes = []
+                        for t in product_felte_obj.taxes_id:
+                            taxes.append(t.id)                        
+                        if recs.line == False:
+                            inv_line = rec.invoice_line_ids.create({           
+                            #'sequence':10,
+                            'product_id':product_felte_obj.id,
+                            'origin':'Cobro por el flete del viaje - ' + acc.name,
+                            'name':'Cobro por el flete del viaje - ' + acc.name,
+                            'company_id':self.company_id.id,
+                            'invoice_id':recs.account.id,
+                            'travels_ids':recs.id,
+                            'account_id':product_felte_obj.categ_id.property_account_income_categ_id.id,
+                            'account_analytic_id':account_analytic.id,
+                            #'analytic_tag_ids':rec.
+                            'quantity':0,
+                            'uom_id':product_felte_obj.uom_id.id,
+                            'price_unit':acc.flete_cliente,
+                            #'discount':0.00,
+                            'invoice_line_tax_ids':[(6, 0, taxes)],
+                            #'price_subtotal':100.00,
+                            #'currency_id':34
+                            })
+
+                            
+                               
+                          
+                    if not account_analytic:
+                        account=self.env['account.analytic.account'].create({                      
+                            'name':acc.unit_id.name,                             
+                            })
+                        if account:
                             taxes = []
                             for t in product_felte_obj.taxes_id:
                                 taxes.append(t.id)                        
                             if recs.line == False:
-                                rec.invoice_line_ids.create({           
+                                inv_line = rec.invoice_line_ids.create({        
                                 #'sequence':10,
                                 'product_id':product_felte_obj.id,
                                 'origin':'Cobro por el flete del viaje - ' + acc.name,
@@ -84,9 +185,9 @@ class AccountInvoice(models.Model):
                                 'invoice_id':recs.account.id,
                                 'travels_ids':recs.id,
                                 'account_id':product_felte_obj.categ_id.property_account_income_categ_id.id,
-                                'account_analytic_id':account_analytic.id,
+                                'account_analytic_id':account.id,
                                 #'analytic_tag_ids':rec.
-                                'quantity':1.00,
+                                'quantity':0,
                                 'uom_id':product_felte_obj.uom_id.id,
                                 'price_unit':acc.flete_cliente,
                                 #'discount':0.00,
@@ -95,49 +196,21 @@ class AccountInvoice(models.Model):
                                 #'currency_id':34
                                 })
 
-                                
-                                   
-                              
-                        if not account_analytic:
-                            account=self.env['account.analytic.account'].create({                      
-                                'name':acc.unit_id.name,                             
-                                })
-                            if account:
-                                taxes = []
-                                for t in product_felte_obj.taxes_id:
-                                    taxes.append(t.id)                        
-                                if recs.line == False:
-                                    rec.invoice_line_ids.create({        
-                                    #'sequence':10,
-                                    'product_id':product_felte_obj.id,
-                                    'origin':'Cobro por el flete del viaje - ' + acc.name,
-                                    'name':'Cobro por el flete del viaje - ' + acc.name,
-                                    'company_id':self.company_id.id,
-                                    'invoice_id':recs.account.id,
-                                    'travels_ids':recs.id,
-                                    'account_id':product_felte_obj.categ_id.property_account_income_categ_id.id,
-                                    'account_analytic_id':account.id,
-                                    #'analytic_tag_ids':rec.
-                                    'quantity':1.00,
-                                    'uom_id':product_felte_obj.uom_id.id,
-                                    'price_unit':acc.flete_cliente,
-                                    #'discount':0.00,
-                                    'invoice_line_tax_ids':[(6, 0, taxes)],
-                                    #'price_subtotal':100.00,
-                                    #'currency_id':34
-                                    })
-                                    
-                                    
-
 
 
            
             for x in rec.travel_ids:
                 x.write({'line':True})
 
-                for acc in x.travel_id:
-                    acc.write({'account_id':x.account.id})
-                   
+                x.travel_id.write({'account_id':x.account.id})
+
+
+    @api.multi
+    def write(self, values):
+        for rec in self:
+            res = super(AccountInvoice, self).write(values) 
+            rec.update_invoice_lines()
+            # rec.calcula_inv_lines()
             return res
 
     
@@ -188,7 +261,7 @@ class AccountInvoice(models.Model):
                                     'company_id':self.company_id.id,
                                     'invoice_id':rec.account.id,
                                     'travels_ids':rec.id,
-                                    'account_id':rec.product_felte_obj.categ_id.property_account_income_categ_id.id,
+                                    'account_id':product_felte_obj.categ_id.property_account_income_categ_id.id,
                                     'account_analytic_id':account_analytic.id,
                                     #'analytic_tag_ids':rec.
                                     'quantity':1.00,
